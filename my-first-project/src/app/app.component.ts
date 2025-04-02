fc585c1c8604fb3457e987001287e632acf4ee40
AaravAbraham <w0848228@myscc.ca>
2025-04-01 21:20:46 -0400

    Created starter Angular file and completed Assignment 1

---
+++ my-first-project/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'my-first-project';
+    myName: string = "Your Name";
+    myCourse: string = "MAD 307 - JavaScript Frameworks";
+  }
+  
