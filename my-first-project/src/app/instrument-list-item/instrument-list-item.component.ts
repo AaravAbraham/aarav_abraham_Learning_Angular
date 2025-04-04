df994f61f0b528daab11fe6b7e8ca1726bfb0a2e
AaravAbraham <w0848228@myscc.ca>
2025-04-04 02:30:14 -0400

    Assignment 3 done

--- my-first-project/src/app/instrument-list-item/instrument-list-item.component.ts
+++ my-first-project/src/app/instrument-list-item/instrument-list-item.component.ts
@@ -1,13 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../models/product';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-instrument-list-item',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './instrument-list-item.component.html',
   styleUrl: './instrument-list-item.component.scss'
 })
 export class InstrumentListItemComponent {
+
   @Input() product! : Product;
 
 }
--- my-first-project/src/app/instrument-list/instrument-list.component.ts
+++ my-first-project/src/app/instrument-list/instrument-list.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { Product } from '../models/product';
+import { NgFor } from '@angular/common';
 import { InstrumentListItemComponent } from '../instrument-list-item/instrument-list-item.component';
 
 @Component({
   selector: 'app-instrument-list',
   standalone: true,
-  imports: [],
+  imports: [NgFor, InstrumentListItemComponent],
   templateUrl: './instrument-list.component.html',
   styleUrl: './instrument-list.component.scss'
 })
+
 export class InstrumentListComponent {
    accesories : Product[]= [
       { id: 1, name: 'Bag', type: 'String', price: 49 },
