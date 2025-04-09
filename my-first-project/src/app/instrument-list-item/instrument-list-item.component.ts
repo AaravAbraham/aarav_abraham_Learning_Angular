0b2215410c701569c220ac14d2edc8a324fbc7e6
AaravAbraham <w0848228@myscc.ca>
2025-04-09 16:57:29 -0400

    Assignment 4 complete

--- my-first-project/src/app/instrument-list-item/instrument-list-item.component.ts
+++ my-first-project/src/app/instrument-list-item/instrument-list-item.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
-import { NgForOf } from '@angular/common';
+import { NgFor } from '@angular/common';
 import { Product } from '../models/product';
 
 @Component({
   selector: 'app-instrument-list-item',
-  imports:[NgForOf],
+  imports:[],
   templateUrl: './instrument-list-item.component.html',
-  styleUrl: './instrument-list-item.component.scss'
+  styleUrl: './instrument-list-item.component.scss',
+  standalone: true
 })
 export class InstrumentListItemComponent {
   @Input() product!: Product;
--- my-first-project/src/app/instrument-list/instrument-list.component.ts
+++ my-first-project/src/app/instrument-list/instrument-list.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { Product } from '../models/product';
 import { MOCK_CONTENT } from '../data/mock-content'; 
+import { NgFor } from '@angular/common';
+import { InstrumentListItemComponent } from '../instrument-list-item/instrument-list-item.component';
 
 @Component({
   selector: 'app-instrument-list',
   templateUrl: './instrument-list.component.html',
   styleUrls: ['./instrument-list.component.scss'],
-  imports: [] 
+  imports: [NgFor, InstrumentListItemComponent],
+  standalone: true
 })
+
 export class InstrumentListComponent {
   accessories: Product[] = MOCK_CONTENT; 
 }
