35aa651d0f838500b2bce8915ac7143172ebb681
AaravAbraham <w0848228@myscc.ca>
2025-04-10 05:40:42 -0400

    s

--- my-first-project/src/app/instrument-list-item/instrument-list-item.component.ts
+++ my-first-project/src/app/instrument-list-item/instrument-list-item.component.ts
@@ -1,17 +1,17 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common'; // ✅ Import this
-import { PriceTagPipe } from '../pipes/price-tag.pipe'; // ✅ Import your custom pipe
+import { CommonModule } from '@angular/common';
+import { PriceTagPipe } from '../pipes/price-tag.pipe'; 
 import { Product } from '../models/product';
 import { HoverHighlightDirective } from '../directives/hover-highlight.directive';
 
 @Component({
   selector: 'app-instrument-list-item',
   standalone: true,
-  imports: [CommonModule, PriceTagPipe,HoverHighlightDirective], // ✅ Add both
+  imports: [CommonModule, PriceTagPipe,HoverHighlightDirective], 
   templateUrl: './instrument-list-item.component.html',
   styleUrls: ['./instrument-list-item.component.scss']
 })
 export class InstrumentListItemComponent {
   @Input() product!: Product;
-  today: Date = new Date(); // for the date pipe
+  today: Date = new Date(); 
 }
