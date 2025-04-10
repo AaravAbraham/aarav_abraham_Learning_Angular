dd2a7a889f7367e03386d9b4153c9057e41c30c7
AaravAbraham <w0848228@myscc.ca>
2025-04-10 05:38:58 -0400

    started assignment 9

--- my-first-project/src/app/instrument-list-item/instrument-list-item.component.ts
+++ my-first-project/src/app/instrument-list-item/instrument-list-item.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common'; // ✅ Import this
 import { PriceTagPipe } from '../pipes/price-tag.pipe'; // ✅ Import your custom pipe
 import { Product } from '../models/product';
+import { HoverHighlightDirective } from '../directives/hover-highlight.directive';
 
 @Component({
   selector: 'app-instrument-list-item',
   standalone: true,
-  imports: [CommonModule, PriceTagPipe], // ✅ Add both
+  imports: [CommonModule, PriceTagPipe,HoverHighlightDirective], // ✅ Add both
   templateUrl: './instrument-list-item.component.html',
   styleUrls: ['./instrument-list-item.component.scss']
 })
